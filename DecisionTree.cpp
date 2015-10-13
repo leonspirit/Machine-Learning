c0a17292639816375ef5f3f990599c91f050bf3e
louis <louis@louis-desktop.(none)>
2015-10-13 09:50:24 +0700

    Build DecisionTree

--- DecisionTree.cpp
+++ DecisionTree.cpp
@@ -32,8 +32,47 @@ node* build(node *p, table data_now, int record, int attribute)
 	if(homogen)
 	{
 		node *temp = new node();
+		temp->attr_pick=-1;
 		temp->label=data_now[0][attribute-1];
 		return temp;
+	}
+	
+	double maks=-99; int ans=-1;
+	for(int x=0;x<attribute-1;x++)
+	{
+		int arr[record], id=0, kless[record];
+		for(int y=0;y<data_now.size();y++)
+		{
+			arr[id++]=data_now[y][x];
+			kless[id++]=data_now[y][attribute-1];
+		}
+		double info_gain = infogain();
+		if(info_gain > maks){maks=info_gain; ans=x;}
+	}
+	
+	bool flag[record];
+	memset(flag,false,sizeof flag);
+	node *temp = new node();
+	temp->attr_pick=ans;
+	
+	for(int x=0;x<record;x++)
+	{
+		if(flag[x]==false)
+		{
+			vector<string>child_data;
+			flag[x]=true;
+			for(int y=x;y<record;y++)
+			{
+				if(data_now[x][ans]==data_now[y][ans])
+				{
+					flag[y]=true;
+					for(int z=0;z<attribute;z++)
+					{
+						if(z!=ans)child_data.pb(data_now[y][z]);
+					}
+				}
+			}
+		}
 	}
 	
 	
