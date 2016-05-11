8d9a2f2e565a8d557055e7321f5a315d50fb728c
leonspirit <archmage_louis@yahoo.co.id>
2016-05-11 21:18:01 +0700

    Docs and Paper added

--- FP DataMining/INBF.py
+++ FP DataMining/INBF.py
@@ -24,6 +24,8 @@ record = 569
 offset = 2
 L = 5
 K = 5
+
+#vector weight (W), inisialisasi random
 vecW = np.random.uniform(low=0.0, high=1.0, size=field-2 )
 
 with open('wdbc.csv','rb') as csvfile:
@@ -46,14 +48,17 @@ for j in range(offset,field):
         maks = max(maks, cancerData[i][j])
         mins = min(mins, cancerData[i][j])
     
+    #normalisasi data agar data pada range 0..1
     for i in range(1,record+1):
         cancerData[i][j] = (cancerData[i][j] - mins) / (maks - mins)
     
 #feature selection IBNF
 for t in range(itr):
+    #pick random instance
     selectedRecord = random.randint(1,record)
     dist = [(0,0) for x in range(record+1)] 
     
+    #calculate distance using vector W
     for i in range(1,record+1):        
         temp = dist[i]
         temp = list(temp)
@@ -66,18 +71,21 @@ for t in range(itr):
             jarak = jarak * jarak
             
             temp = dist[i]
-            #print temp
             temp = list(temp)
             temp[0] = temp[0] + (jarak * vecW[j - offset])
             temp = tuple(temp)
             dist[i] = temp
         
+    #sort the distance to find K nearest neighbour and L farthest neighbour
     dist[1:record] = sorted(dist[1:record], key=comparator)
     vecFC = [0 for x in range(field-2)]
     vecFS = [0 for x in range(field-2)]
     vecU = [0 for x in range(field-2)]
     
+    #calculate vector U
     for i in range(offset,field):
+        
+        #Feature Compactness for feature i
         for j in range(offset, K+offset):
             neighbour = int(dist[j][1])
             cost = cancerData[selectedRecord][i]-cancerData[neighbour][i]
@@ -85,7 +93,8 @@ for t in range(itr):
             vecFC[i-offset] = vecFC[i-offset] + cost
         vecFC[i-offset] = vecFC[i-offset]*-1
         vecFC[i-offset] = vecFC[i-offset]/K
-            
+        
+        #Feature Separability for feature i
         for j in range(record-L+1, record):
             neighbour = int(dist[j][1])
             cost = cancerData[selectedRecord][i]-cancerData[neighbour][i]
@@ -93,13 +102,15 @@ for t in range(itr):
             vecFS[i-offset] = vecFS[i-offset] + cost
         vecFS[i-offset] = vecFS[i-offset]/L
         
+        #calculate vector U, and update vector W
         vecU[i-offset] = vecFC[i-offset] + vecFS[i-offset]
         vecW[i-offset] = vecW[i-offset] + (alpha * (vecU[i-offset] - vecW[i-offset]) )
     
+    #decrease learning rate for each iteration
     alpha = alpha * 0.9
-    #print "%f %f %f %f %f %f %f\n" % (vecU[0],vecW[0], vecU[1],vecW[1], vecU[2],vecW[2], alpha)
-
-print vecW
+ 
+#uncomment to print weight vector (W)   
+#print vecW
 featureSelection = [(0,0) for x in range(field-2)]
 
 for i in range(len(vecW)):
@@ -113,4 +124,13 @@ for i in range(len(vecW)):
     featureSelection[i] = temp
     
 featureSelection = sorted(featureSelection, key=comparator, reverse=True)
-print featureSelection
+
+#uncomment to print sorted weight vector and the attributes
+#print featureSelection
+
+features = 10
+for i in range(features):
+    idx = featureSelection[i][1]
+    print "Weight : %f, Attribute : %s" % (featureSelection[i][0], cancerData[0][idx+offset-1])
+    
+    
